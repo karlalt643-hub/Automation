b0fd78f7463a958caf76f32235412079e31e8c30
karlalt643-hub <karlalt643@gmail.com>
2025-11-26 13:31:33 +0100

    Add files via upload

--- _buildManifest.js
+++ _buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-91a5938854a6f402.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
